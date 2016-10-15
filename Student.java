734309f7ad1f6984b45c88f97995611a33755d31
rknakata <rknakata@hawaii.edu>
2016-10-15 02:18:04 +0000

    Add fullname method to Student.java.

---
+++ Student.java
@@ -0,0 +1,20 @@
+public class Student{
+  int age;
+  float gpa;
+  String firstName;
+  String lastName;
+  String major;
+  public Student(){
+
+  }
+
+  public Student(String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  public String fullName(){
+    return this.firstName + " " + this.lastName;
+  }
+}
